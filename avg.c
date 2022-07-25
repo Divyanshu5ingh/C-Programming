cc8ad5fc94d835ba5e1fb418767cf8ab7737dba4
Divyanshu Kumar <90168140+Divyanshu5ingh@users.noreply.github.com>
2022-07-25 23:55:03 +0530

    Add files via upload

---
+++ avg.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int avg(int[], int);
+
+void main()
+{
+    int average;
+
+    int marks[5] = {10, 15, 20, 30, 45};
+    average = avg(marks, 5);
+    printf("AVg is %d: \n", average);
+}
+int avg(int marks[], int a)
+{
+    int i, sum = 0, average=0;
+    for(i = 0; i<a; i++)
+    {
+        sum = sum + marks[i];
+    }
+    average = sum/a;
+    return average;
+}
