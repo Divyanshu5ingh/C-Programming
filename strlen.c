900c9473d9858ea0ad9203dc54a0d4061c393e0e
Divyanshu Kumar <90168140+Divyanshu5ingh@users.noreply.github.com>
2022-07-29 01:54:27 +0530

    Add files via upload

---
+++ strlen.c
@@ -0,0 +1,12 @@
+// counting string length in C language using Strlen function
+#include<stdio.h>
+#include<string.h>
+void main()
+{   
+    char * p = "Hello";
+    int a;
+
+    a = strlen(p);
+    printf("%d", a);
+
+}
